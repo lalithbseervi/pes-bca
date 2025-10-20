daca654ee7414f1c481c7174ba4e1dcdf4edce51
lalithbseervi <lcoding20@gmail.com>
2025-10-20 13:51:53 +0530

    seo: update shell script to include lastmod of template files

--- update-updated-field.sh
+++ update-updated-field.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 find content -name '*.md' | while read file; do
-  lastmod=$(date -r "$file" +"%Y-%m-%dT%H:%M:%S%:z")
+  # Get lastmod of markdown file
+  md_lastmod=$(date -r "$file" +"%Y-%m-%dT%H:%M:%S%:z")
+
+  # Extract template or subject_template from front matter
+  template=$(awk -F'=' '/^template = / {gsub(/"/, "", $2); print $2}' "$file" | xargs)
+  subject_template=$(awk -F'=' '/^subject_template = / {gsub(/"/, "", $2); print $2}' "$file" | xargs)
+
+  # Determine template path
+  tpl=""
+  if [ -n "$subject_template" ]; then
+    tpl="templates/$subject_template"
+  elif [ -n "$template" ]; then
+    tpl="templates/$template"
+  fi
+
+  # Get lastmod of template if it exists
+  if [ -n "$tpl" ] && [ -f "$tpl" ]; then
+    tpl_lastmod=$(date -r "$tpl" +"%Y-%m-%dT%H:%M:%S%:z")
+  else
+    tpl_lastmod=""
+  fi
+
+  # Choose the most recent lastmod
+  if [ -n "$tpl_lastmod" ] && [[ "$tpl_lastmod" > "$md_lastmod" ]]; then
+    lastmod="$tpl_lastmod"
+  else
+    lastmod="$md_lastmod"
+  fi
+
+  # Remove any existing updated field and insert the new one
   awk -v lastmod="$lastmod" '
     BEGIN {in_front=0; inserted=0; in_extra=0}
-    # Start of TOML front matter
     /^(\+\+\+|---)$/ {
       print
       if (!in_front) { in_front=1; next }
-      # End of front matter
-      in_front=0
-      in_extra=0
-      next
+      in_front=0; in_extra=0; next
     }
-    # Remove any existing updated field in front matter
     in_front && /^updated = / { next }
-    # Insert after date
     in_front && /^date = / {
       print
       print "updated = \""lastmod"\""
       inserted=1
       next
     }
-    # Insert as first entry under [extra]
     in_front && /^\[extra\]/ {
       print
       in_extra=1
@@ -34,7 +56,6 @@ find content -name '*.md' | while read file; do
     }
     { print }
     END {
-      # If still in front matter and not inserted, add updated at the end of front matter
       if (in_front && !inserted) {
         print "updated = \""lastmod"\""
       }
