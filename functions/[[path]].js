8928f40660084becf4b75104f59ce84dceff8ada
lalithbseervi <lcoding20@gmail.com>
2025-10-22 22:59:01 +0530

    fix: update cf-func to handle conversion of /static/ to /pdf-viewer/

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,8 +4,18 @@ export async function onRequest(context) {
   
   // PDF Protection Logic
   if (url.pathname.includes('.pdf')) {
+      // Redirect /static/ URLs to PDF viewer
+    if (url.pathname.startsWith('/static/')) {
+      const filePath = url.pathname.replace('/static/', '/');
+      const pdfUrl = new URL(url.origin + '/pdf-viewer/');
+      pdfUrl.searchParams.set('file', filePath);
+      const filename = filePath.split('/').pop().replace(/\.[^/.]+$/, '');
+      pdfUrl.searchParams.set('title', filename);
+      return Response.redirect(pdfUrl.toString(), 302);
+    }
+
     const referer = request.headers.get('referer') || request.headers.get('Referer');
-    
+  
     // Temporary debug logging
     console.log('PDF Request:', {
       url: url.pathname,
