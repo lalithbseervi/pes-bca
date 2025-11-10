835116251d50fde52db54a64fb260d276e1704ca
lalithbseervi <lcoding20@gmail.com>
2025-11-10 14:10:04 +0530

    bugfix: applyFilters is not defined

--- cors-proxy/src/utils/cookies.js
+++ cors-proxy/src/utils/cookies.js
@@ -13,7 +13,6 @@ function isLocalFromRequest(request) {
 
 export function makeCookie(name, value, maxAgeSec, request) {
   const isLocal = isLocalFromRequest(request)
-  console.log(`isLocal: ${isLocal}`)
   const attrs = [
     `${name}=${value}`,
     `Max-Age=${maxAgeSec}`,
